9b21600ca7dc10a90ac1c1ce2c8e52cd98c13142
Mehul Rana <mehul.2rana.2005@gmail.com>
2025-04-26 11:52:08 +0530

    update resume

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -74,7 +74,7 @@ const Home = () => {
             </a>
           </div>
 
-          <button className="resume"><a href="https://drive.google.com/file/d/1VUnx-RPHmDAVV0HDN8bP4my0-23CyLmu/view?usp=drivesdk">see my resume</a></button>
+          <button className="resume"><a href="https://drive.google.com/file/d/1Z_FHmhY-Zl6CPf0jIbzFtfvjWBXi4qg-/view?usp=drivesdk">see my resume</a></button>
         </div>
 
         <div className="home-image">
